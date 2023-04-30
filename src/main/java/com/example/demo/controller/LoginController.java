23a200e65f1db4b09483c75341778e825c6bf05c
gyoon <xmas6904201@gmail.com>
2023-04-30 22:32:56 +0900

    회원가입 폼 프론트 완료 , 백엔드 검증완료

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,10 +1,36 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Member;
+import com.example.demo.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.naming.Binding;
 
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/member-add")
+    public String add(@ModelAttribute("member") Member member) {
+        return "/member/addmember";
+    }
+
+    @PostMapping("/member-add")
+    public String save(@Validated @ModelAttribute("member") Member member, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/member/addmember";
+        }
+        memberRepository.save(member);
+        return "redirect:/show";
+
+    }
 }
--- src/main/java/com/example/demo/repository/member/MemberRepository.java
+++ src/main/java/com/example/demo/repository/member/MemberRepository.java
@@ -2,17 +2,45 @@ package com.example.demo.repository.member;
 
 
 import com.example.demo.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.*;
 
 @Repository
+@Slf4j
 public class MemberRepository implements IMemberRepo {
     private final static Map<Integer, Member> memberRepo = new HashMap<>();
     private static Integer sequence = 0;
 
 
+    @Override
+    public Member save(Member member) {
+        member.setId(sequence);
+        log.info("memberId => {}", member.getId());
+        return memberRepo.put(member.getId(), member);
+    }
 
+    @Override
+    public Member findById(Integer memberId) {
+        return memberRepo.get(memberId);
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(memberRepo.values());
+    }
+
+    @Override
+    public Optional<Member> findLoginId(String loginId) {
+        return this.findAll()
+                .stream()
+                .filter(member -> member.getLoginId().equals(loginId))
+                .findFirst();
+    }
+
+    @Override
+    public void reset() {
+        memberRepo.clear();
+    }
 }
