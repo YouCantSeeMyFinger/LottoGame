317abe54bf812cd6aef1c26c0929fc173c45d160
gyoon <xmas6904201@gmail.com>
2023-05-01 21:15:00 +0900

    쿠키를 사용하여 로그인시 문제발생
    문제 : 해커가 악의적으로 쿠키를 변경하여 다른사용자의 아이디로 로그인할 수 있다는 지적받음. 해결방안 세션처리

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,15 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.Member;
 import com.example.demo.repository.member.MemberRepository;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
